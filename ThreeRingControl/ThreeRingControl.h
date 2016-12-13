71a44da02c9ada01bce44e2170a5b599627f16d9
krunal_smartsense <krunal.smartsensesolutions@gmail.com>
2016-12-13 15:25:36 +0530

    initial commit

---
+++ ThreeRingControl/ThreeRingControl.h
@@ -0,0 +1,19 @@
+//
+//  ThreeRingControl.h
+//  ThreeRingControl
+//
+//  Created by smartSense on 13/12/16.
+//  Copyright © 2016 smartSense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThreeRingControl.
+FOUNDATION_EXPORT double ThreeRingControlVersionNumber;
+
+//! Project version string for ThreeRingControl.
+FOUNDATION_EXPORT const unsigned char ThreeRingControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThreeRingControl/PublicHeader.h>
+
+
